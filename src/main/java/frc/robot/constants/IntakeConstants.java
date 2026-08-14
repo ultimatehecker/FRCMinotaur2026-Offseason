65fdc133c1d62c81db33d5e16853a9ec29afee40
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2026-08-14 16:39:43 -0400

    renamed to more logical names

--- src/main/java/frc/robot/constants/IntakeConstants.java
+++ src/main/java/frc/robot/constants/IntakeConstants.java
@@ -6,7 +6,8 @@ import frc.robot.Constants;
 
 public class IntakeConstants {
     public class Slam {
-        public static final MinoCANDevice kDevice = new MinoCANDevice(14, Constants.kRioBus);
+        public static final MinoCANDevice kKrakenX60 = new MinoCANDevice(14, Constants.kRioBus);
+        public static final MinoCANDevice kCANCoder = new MinoCANDevice(14, Constants.kRioBus);
 
         public static final double kP = 10.0;
         public static final double kI = 0.0;
@@ -21,10 +22,12 @@ public class IntakeConstants {
         public static final double kMotorStatorCurrentLimit = 110;
         public static final double kMotorSupplyCurrentLimit = 55;
         public static final DCMotor kSimulatedGearbox = DCMotor.getKrakenX60Foc(1);
+
+        public static final double kAbsoluteEncoderOffsetRadians = -2.4908;
     }
 
     public class Roller {
-        public static final MinoCANDevice kDevice = new MinoCANDevice(15, Constants.kRioBus);
+        public static final MinoCANDevice kFalcon500 = new MinoCANDevice(15, Constants.kRioBus);
 
         public static final double kP = 10.0;
         public static final double kI = 0.0;
