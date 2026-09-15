b533ed64f220298e6c37a0914a50324c7039d80d
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2026-09-15 16:01:49 -0400

    added sysid routines

--- src/main/java/frc/robot/subsystems/floor/Floor.java
+++ src/main/java/frc/robot/subsystems/floor/Floor.java
@@ -1,11 +1,17 @@
 package frc.robot.subsystems.floor;
 
+import static edu.wpi.first.units.Units.Volts;
+
 import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
+
+import org.littletonrobotics.junction.Logger;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 import frc.minolib.advantagekit.LoggedTracer;
 import frc.robot.subsystems.rollers.RollerSystem;
 import frc.robot.subsystems.rollers.RollerSystemIO;
@@ -17,9 +23,26 @@ public class Floor extends SubsystemBase {
 
     @Setter private BooleanSupplier coastOverride = () -> false;
 
+    private final SysIdRoutine sysIdRoutine;
+
     public Floor(RollerSystemIO io) {
         rollerSystem = new RollerSystem("Floor", "Floor", io);
         rollerSystem.setCoastOverride(coastOverride);
+
+        sysIdRoutine = new SysIdRoutine(
+            new SysIdRoutine.Config(
+                null,
+                null,
+                null,
+                state -> Logger.recordOutput("SysId/Floor/State", state.toString())
+            ),
+            new SysIdRoutine.Mechanism(
+                voltage -> rollerSystem.runOpenLoop(voltage.in(Volts)),
+                null,
+                this,
+                "Floor"
+            )
+        );
     }
 
     @Override
@@ -34,5 +57,13 @@ public class Floor extends SubsystemBase {
             () -> rollerSystem.runOpenLoop(0.0),
             this
         );
+    }
+
+    public Command sysIdQuasistatic(Direction direction) {
+        return sysIdRoutine.quasistatic(direction);
+    }
+
+    public Command sysIdDynamic(Direction direction) {
+        return sysIdRoutine.dynamic(direction);
     }
 }
